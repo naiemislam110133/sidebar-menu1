a2ce8cdc85941252b3c6a30f3e3ded76ba47e3d1
Naiem Islam <naiemislam1547@gmail.com>
2023-10-13 21:07:11 +0600

    not work jquery

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-$(".menu > ul > li").click(function(e){
-    // remove active from already active
-    $(this).siblings().removeClass("active");
-    // add active to clicked
-    $(this).toggleClass("active");
-});
+// $(".menu > ul > li").click(function(e){
+//     // remove active from already active
+//     $(this).siblings().removeClass("active");
+//     // add active to clicked
+//     $(this).toggleClass("active");
+//     // if has sub menu open it 
+//     $(this).find("ul").slideToggle();
+// });
